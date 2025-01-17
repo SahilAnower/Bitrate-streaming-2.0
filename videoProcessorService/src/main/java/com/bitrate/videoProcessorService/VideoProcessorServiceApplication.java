a95a9ad65d0281850c7de8e0e9b4735459edbbb3
SahilAnower <sahilanower2@gmail.com>
2025-01-18 05:03:29 +0530

    Added video processor service

---
+++ videoProcessorService/src/main/java/com/bitrate/videoProcessorService/VideoProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bitrate.videoProcessorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ videoProcessorService/src/test/java/com/bitrate/videoProcessorService/VideoProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitrate.videoProcessorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
