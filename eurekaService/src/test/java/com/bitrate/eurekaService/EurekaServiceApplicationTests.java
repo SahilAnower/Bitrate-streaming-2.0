94f5c26fd4df3a8ae6775ce731fdf52cb2868681
SahilAnower <sahilanower2@gmail.com>
2025-01-18 04:55:19 +0530

    Added gateway, eureka, and user services

---
+++ eurekaService/src/test/java/com/bitrate/eurekaService/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitrate.eurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
